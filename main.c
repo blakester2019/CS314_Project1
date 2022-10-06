78a0d5488394c192f6274e7c02ec5fad2d178d60
blakester2019 <blakester2019@gmail.com>
2022-10-06 18:11:17 -0500

    Created Parsing Functionality

--- main.c
+++ main.c
@@ -1,10 +1,34 @@
 #include "stdio.h"
 #include "stdlib.h"
+#include <string.h>
+
+char* retBuffer = NULL;
+size_t buffSize;
+
+typedef struct format
+{
+    long int numItems;
+    char* argv[];
+} format;
+
+void parsing(char* retBuffer, format* junk);
 
 int main(int argc, char* argv)
 {
-    printf("go kys\n");
-    printf("Test bro\n");
-    printf("Test 2 bro\n");
+    getline(&retBuffer, &buffSize, stdin);
+    format* junk = malloc(sizeof(format));
+    parsing(retBuffer, junk);
+    printf("%s\n", junk->argv[0]);
     return 0;
+}
+
+void parsing(char* retBuffer, format* junk)
+{
+    char* token;
+    junk->numItems = (long)malloc(sizeof(int));
+    junk->numItems = 0;
+    while((token = strsep(&retBuffer, " ")) != NULL)
+    {
+        junk->argv[junk->numItems++] = token;
+    }
 }
