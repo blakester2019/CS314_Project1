0cf8d97fba85fbd000e51a84a812c8724c68de62
Blake Rhodes <blakester2019@gmail.com>
2022-10-26 23:53:09 -0500

    inputRedirect needs fixed. Added rc capabilities

--- main.c
+++ main.c
@@ -19,57 +19,136 @@ size_t buffSize;
 command* cmd;
 
 // Prototypes
-void parseCommands(int size);
 int createCommandInstances(char* retBuffer);
 void getCommand();
-int knownCommands(char** args);
+int knownCommands();
 void fillBlank(char* arr[], int size);
 void viewCommands(int size);
 void performPipe(int leftCommandIndex);
 void outputRedirect(int leftCommandIndex);
+void outputAppendRedirect(int leftCommandIndex);
+void inputRedirect(int leftCommandIndex);
 int getSize(char* argv[]);
 char** formatArgs(char* args[], int max, int commandNum);
+void executeCommands(int commandSize);
+void executeRC();
 
 // ----- MAIN -----
 int main(int argc, char* argv[])
 {
+    // Start by executing commands from RC file
+    executeRC();
+
+    // Begin taking commands from user
     while (1)
     {
         // Get User Input
         getCommand();
 
-        // Get Size of Commands
-        int size = createCommandInstances(retBuffer);
+        // Get How Many Commands To Run
+        int commandSize = createCommandInstances(retBuffer);
 
-        // List Commands
-        //viewCommands(size);
-
-        /*
-        int size2 = getSize(cmd[1].args);
-        printf("Size2: %d\n", size2);
-        cmd[1].args[0][strcspn(cmd[1].args[0], "\n")] = 0;
-        for (int i = 0; i < size2; i++)
+        if (commandSize > 0)
         {
-            printf("args2[%d]: %s\n", i, cmd[1].args[i]);
+            executeCommands(commandSize);
         }
-        */
-        outputRedirect(0);
+
+        // outputAppendRedirect(0);
     }
     return 0;
 }
 
-void parseCommands(int size)
+//
+// executeCommands: executes the global array of commands
+// returns void
+//
+void executeCommands(int commandSize)
 {
+    // If only 1 command, execute
+    if (commandSize == 1)
+    {
+        // Check Known Commands
+        knownCommands();
+
+        int pid = fork();
+        if (pid == 0)
+        {
+            char** formattedArgs = formatArgs(cmd[0].args, 10, 0);
+            execvp(cmd[0].cmd, formattedArgs);
+            printf("\n");
+        }
+        waitpid(pid, NULL, 0);
+        return;
+    }
+
+    // 2 Commands, No Sequencing
+    if (commandSize == 2)
+    {
+        if (strcmp(cmd[0].separator, "|") == 0)
+            performPipe(0);
+        else if (strcmp(cmd[0].separator, ">") == 0)
+            outputRedirect(0);
+        else if (strcmp(cmd[0].separator, ">>") == 0)
+            outputAppendRedirect(0);
+        else if (strcmp(cmd[0].separator, "<") == 0)
+            inputRedirect(0);
+        else
+            printf("Invalid Separator\n");
+        return;
+    }
+
+    // After this should handle sequencing
     
 }
 
 //
-// parse: parses input and populates a "command" type
+// executeRC: executes the commands found in pish.rc
 // returns void
+//
+void executeRC()
+{
+    FILE* rc;
+    int buffSize = 255;
+    char buffer[buffSize];
+
+    // Open the file
+    rc = fopen("pish.rc", "r");
+
+    // Check if pish.rc was opened
+    if (rc == NULL)
+    {
+        printf("pish.rc was not found\n");
+        return;
+    }
+
+    // Execute pish.rc line by line
+    while (fgets(buffer, buffSize, rc))
+    {
+        buffer[strlen(buffer)-1] = '\0';
+
+        int commandSize = createCommandInstances(buffer);
+        if (commandSize > 0)
+        {
+            printf("Executing: %s\n", buffer);
+            executeCommands(commandSize);
+        }
+    }
+
+    // Close pish.rc
+    fclose(rc);
+}
+
+//
+// createCommandInstances: populates the global array of commands
+// returns number of commands to be run
 //
 int createCommandInstances(char* retBuffer)
 {
-    char* backupBuffer[30];
+    // If input is empty, create no instances
+    if (strcmp(retBuffer, "") == 0)
+        return 0;
+
+    char* backupBuffer[30]; // retBuffer becomes null after reading tokens. This is a backup of retBuffer
     char* token;
     int instancesToCreate = 1;
 
@@ -82,7 +161,8 @@ int createCommandInstances(char* retBuffer)
         if (
             strcmp(token, "|") == 0 ||
             strcmp(token, "<") == 0 ||
-            strcmp(token, ">") == 0
+            strcmp(token, ">") == 0 ||
+            strcmp(token, ">>") == 0 
         ) 
         {
             instancesToCreate++;
@@ -90,6 +170,7 @@ int createCommandInstances(char* retBuffer)
         backupBuffer[i] = token;
         i++;
     }
+
     // Malloc Space for Instances
     cmd = malloc(sizeof(command) * instancesToCreate);
     for(i = 0; i < instancesToCreate; i++)
@@ -105,6 +186,8 @@ int createCommandInstances(char* retBuffer)
     int currCommandIndex = 0;
     int currArgIndex = 0;
     int j = 0;
+
+    // Begin creating command instances
     while (backupBuffer[j] != "NULL")
     {
         if (initialCommand == 1)
@@ -118,7 +201,8 @@ int createCommandInstances(char* retBuffer)
             if (
             strcmp(backupBuffer[j], "|") == 0 ||
             strcmp(backupBuffer[j], "<") == 0 ||
-            strcmp(backupBuffer[j], ">") == 0
+            strcmp(backupBuffer[j], ">") == 0 ||
+            strcmp(backupBuffer[j], ">>") == 0 
             ) 
             {
                 cmd[currCommandIndex].separator = backupBuffer[j];
@@ -135,6 +219,7 @@ int createCommandInstances(char* retBuffer)
         j++;
     }
     
+    // Return how many instances need to be created. This is useful during command execution
     return instancesToCreate;
 }
 
@@ -144,8 +229,11 @@ int createCommandInstances(char* retBuffer)
 //
 void getCommand()
 {
+    // Take Input
     printf("pish\%> ");
     getline(&retBuffer, &buffSize, stdin);
+
+    // Remove newline character at the end of the input
     retBuffer[strlen(retBuffer)-1] = '\0';
     fflush(stdin);
 }
@@ -154,14 +242,14 @@ void getCommand()
 // Checks if first argument is a Pish command
 // Returns 1 if a known command was executed and 0 if there was no known cmmand
 //
-int knownCommands(char** args)
+int knownCommands()
 {
     char* knownCommands[] = {"exit", "cd", "help", "pwd"};
     int size = 4;
     int command = 0;
     for (int i = 0; i < size; i++)
     {
-        if (!strcmp(args[0], knownCommands[i]))
+        if (!strcmp(cmd[0].cmd, knownCommands[i]))
             command = i + 1;
     }
     
@@ -194,6 +282,10 @@ int knownCommands(char** args)
     return 1;
 }
 
+//
+// fillBlank: fills an array if size {size} with string, "NULL"
+// returns void
+//
 void fillBlank(char* arr[], int size)
 {
     for (int i = 0; i < size; i++)
@@ -202,6 +294,10 @@ void fillBlank(char* arr[], int size)
     }
 }
 
+//
+// viewCommands: Debugging tool to view each element in the global command array
+// returns void
+//
 void viewCommands(int size)
 {
     for (int i = 0; i < size; i++)
@@ -218,6 +314,10 @@ void viewCommands(int size)
     }
 }
 
+//
+// getSize: returns size of char* array
+// returns size of the array
+//
 int getSize(char* argv[])
 {
     int i = -1;
@@ -225,7 +325,10 @@ int getSize(char* argv[])
     return i - 1;
 }
 
-// FIX ME
+//
+// formatArgs: formats the arguments of a command to work with execvp
+// returns array of the formatted arguments
+//
 char** formatArgs(char* args[], int max, int commandNum)
 {
     int size = getSize(cmd[commandNum].args);
@@ -255,9 +358,12 @@ char** formatArgs(char* args[], int max, int commandNum)
     return newArr;
 }
 
+//
+// performPipe: Performs the pipe function
+// returns void
+//
 void performPipe(int leftCommandIndex)
 {
-    printf("Performing pipe on: %d\n", leftCommandIndex);
     int fd[2];
 
     if (pipe(fd) == -1)
@@ -308,6 +414,10 @@ void performPipe(int leftCommandIndex)
     return;
 }
 
+//
+// outputRedirect: Performs an output redirect (>) on a command
+// returns void
+//
 void outputRedirect(int leftCommandIndex) {
     int pid = fork();
 
@@ -330,3 +440,52 @@ void outputRedirect(int leftCommandIndex) {
 
     waitpid(pid, NULL, 0);
 }
+
+//
+// outputAppendRedirect: Performs an output append redirect (>>) on a command
+// returns void
+//
+void outputAppendRedirect(int leftCommandIndex) {
+    int pid = fork();
+
+    if(pid < 0) {
+        printf("fork no good\n");
+        exit(1);
+    }
+
+    if (pid == 0) {
+        int fd = open(cmd[leftCommandIndex + 1].cmd,
+            O_WRONLY | O_APPEND | O_CREAT,
+            0666);
+        dup2(fd, STDOUT_FILENO);
+        close(fd);
+        char** formattedArgs = formatArgs(cmd[leftCommandIndex].args, 10, leftCommandIndex);
+        execvp(cmd[leftCommandIndex].cmd, formattedArgs);
+    }
+
+    waitpid(pid, NULL, 0);
+}
+
+//
+// inputRedirect: Performs an input redirect (<) on a command
+// returns void
+//
+void inputRedirect(int leftCommandIndex) {
+    int pid = fork();
+
+    if (pid < 0) {
+        printf("fork no good\n");
+        exit(1);
+    }
+
+    if (pid == 0) {
+        int fd = open(cmd[leftCommandIndex + 1].cmd, O_RDONLY);
+        dup2(fd, STDOUT_FILENO);
+        close(fd);
+        char** formattedArgs = formatArgs(cmd[leftCommandIndex].args, 10, leftCommandIndex);
+        execvp(cmd[leftCommandIndex].cmd, formattedArgs);
+    }
+
+    waitpid(pid, NULL, 0);
+}
+
